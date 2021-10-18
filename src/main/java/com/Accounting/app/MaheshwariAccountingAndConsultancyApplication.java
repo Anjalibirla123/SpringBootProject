a9e4f679dba2a68916c1d9096927fa5b9884d885
Anjalibirla123 <47386837+Anjalibirla123@users.noreply.github.com>
2021-10-18 17:34:05 +0530

    Folder structure.

---
+++ src/main/java/com/Accounting/app/MaheshwariAccountingAndConsultancyApplication.java
@@ -0,0 +1,13 @@
+package com.Accounting.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaheshwariAccountingAndConsultancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaheshwariAccountingAndConsultancyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Accounting/app/MaheshwariAccountingAndConsultancyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Accounting.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaheshwariAccountingAndConsultancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
